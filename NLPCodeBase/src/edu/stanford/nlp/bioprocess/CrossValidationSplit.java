2eac74f5ebe027327590e020008776d65f124697
Rose Marie Philip <mailrosep@gmail.com>
2013-02-22 23:26:49 -0800

    Added CrossValidationSplit

---
+++ NLPCodeBase/src/edu/stanford/nlp/bioprocess/CrossValidationSplit.java
@@ -0,0 +1,48 @@
+package edu.stanford.nlp.bioprocess;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.Math;
+/**
+ * Split training file to cross validation splits given the total number of folds and the nth fold of test required
+ * 
+ * @author Rose
+ */
+public class CrossValidationSplit  {
+	List<Example> allExamplesCV;
+	int numFoldsCV;
+	List<ArrayList<Example>> foldsCV;
+	public CrossValidationSplit(List<Example> examples, int numFolds){
+		numFoldsCV = numFolds;
+		allExamplesCV = examples;
+		foldsCV = new ArrayList<ArrayList<Example>>();
+		for (int i = 0 ; i < numFolds ; i++){
+			ArrayList<Example> devCV = new ArrayList<Example>();
+			for (int j = 0 ; j < examples.size()/numFolds ; j++){
+				int elemNum = (i*(int)(Math.floor(examples.size()/numFolds)) + j);
+				if (elemNum == examples.size())
+					break;
+				devCV.add(examples.get(elemNum));
+			}
+			foldsCV.add(devCV);
+		}
+	}
+	public List<Example> GetTrainExamples(int numFold){
+		List<Example> trainCV = new ArrayList<Example>();
+		if (numFold<1 || numFold>numFoldsCV){
+			return trainCV;
+		}
+		trainCV = allExamplesCV;
+		trainCV.removeAll(foldsCV.get(numFold-1));
+		return trainCV;
+	}
+	public List<Example> GetTestExamples(int numFold){
+		List<Example> testCV = new ArrayList<Example>();
+		if (numFold<1 || numFold>numFoldsCV){
+			return testCV;
+		}
+		return foldsCV.get(numFold-1);
+	}
+
+}
+
--- NLPCodeBase/src/edu/stanford/nlp/bioprocess/TriggerPredictionInference.java
+++ NLPCodeBase/src/edu/stanford/nlp/bioprocess/TriggerPredictionInference.java
@@ -1,11 +1,30 @@
 package edu.stanford.nlp.bioprocess;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TriggerPredictionInference {
 	public void baselineInfer(List<Example> examples) {
-		for(Example example:examples) {
-			
+		System.out.println("printing CV");
+		List<Example> tempList = new ArrayList<Example>();
+		for (int i = 0 ; i < 13 ; i++){
+			tempList.add(examples.get(i));
+			System.out.println(examples.get(i).id);
 		}
+		System.out.println("printing CV train for fold 4");
+		CrossValidationSplit CV = new CrossValidationSplit(tempList,4);
+		List<Example> train = CV.GetTrainExamples(4);
+		for (Example t:train){
+			System.out.println(t.id);
+		}
+		System.out.println("printing CV test for fold 1");
+		List<Example> test = CV.GetTestExamples(1);
+		for (Example t:test){
+			System.out.println(t.id);
+		}
+		
+		//for(Example example:examples) {
+			
+		//}
 	}
 }
