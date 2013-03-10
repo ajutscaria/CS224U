64bb9356876ced1ce70cf502b5f47d604c89b791
Rishita Anubhai <rishita@stanford.edu>
2013-03-09 16:36:42 -0800

    SRL Baseline Adding

---
+++ NLPCodeBase/src/edu/stanford/nlp/bioprocess/SRLFeatureFactory.java
@@ -0,0 +1,163 @@
+package edu.stanford.nlp.bioprocess;
+
+
+import java.util.*;
+
+import edu.stanford.nlp.bioprocess.ArgumentRelation.RelationType;
+import edu.stanford.nlp.bioprocess.BioProcessAnnotations.EntityMentionsAnnotation;
+import edu.stanford.nlp.bioprocess.BioProcessAnnotations.EventMentionsAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.*;
+import edu.stanford.nlp.trees.CollinsHeadFinder;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreeCoreAnnotations;
+import edu.stanford.nlp.trees.Trees;
+import edu.stanford.nlp.util.CoreMap;
+import fig.basic.LogInfo;
+
+public class SRLFeatureFactory extends FeatureExtractor {
+	boolean printDebug = false, printAnnotations = false, printFeatures = false;
+	HashMap<String, Integer> relCount = new HashMap<String, Integer>();
+
+    public FeatureVector computeFeatures(CoreMap sentence, String tokenClass, Tree entity,  Tree event) {
+	    //Tree event = eventMention.getTreeNode();
+		List<String> features = new ArrayList<String>();
+	
+		Tree root = sentence.get(TreeCoreAnnotations.TreeAnnotation.class);
+		boolean dependencyExists = Utils.isNodesRelated(sentence, entity, event);
+		
+		features.add("EntPOSDepRel=" + entity.value() + ","  + dependencyExists);
+		features.add("EntHeadEvtPOS="+Utils.findCoreLabelFromTree(sentence, entity).lemma() + "," + event.preTerminalYield().get(0).value());
+		features.add("PathEntToEvt=" + Trees.pathNodeToNode(event, entity, root));
+		features.add("EntHeadEvtHead=" + entity.headTerminal(new CollinsHeadFinder()) + "," + event.getLeaves().get(0));
+		features.add("EntNPAndRelatedToEvt=" + (entity.value().equals("NP") && Utils.isNodesRelated(sentence, entity, event)));
+		
+		//features.add("EntPOSEntHeadEvtPOS=" + entity.value() + "," + entity.headTerminal(new CollinsHeadFinder()) + "," + event.preTerminalYield().get(0).value());
+		//features.add("EntPOSEvtPOSDepRel=" + entity.value() + "," +event.preTerminalYield().get(0).value() + ","  + dependencyExists);
+		//features.add("EntPOSEntParentPOSEvtPOS=" + entity.value() + "," + entity.headTerminal(new CollinsHeadFinder()) + "," + event.preTerminalYield().get(0).value());
+		//features.add("EntLastWordEvtPOS="+leaves.get(leaves.size()-1)+","+event.preTerminalYield().get(0).value());
+		//features.add("PathEntToAncestor="+Trees.pathNodeToNode(entity, Trees.getLowestCommonAncestor(entity, event, root), root));
+		//features.add("PathEntToRoot="+Trees.pathNodeToNode(entity, root, root));
+		//features.add("EntParentPOSEvtPOS=" + entity.headTerminal(new CollinsHeadFinder()) + "," + event.preTerminalYield().get(0).value());
+		
+		
+		
+		//This feature did not work surprisingly. Maybe because the path from ancestor to event might lead to a lot of different variations.
+		//features.add("PathAncestorToEvt="+Trees.pathNodeToNode(Trees.getLowestCommonAncestor(entity, event, root), event, root));
+		//This is a bad feature too.
+		//features.add("EvtPOSDepRel=" + event.preTerminalYield().get(0).value() + ","  + dependencyExists);
+		//Not a good feature too.
+		//features.add("EntPOSEvtPOS=" + entity.value() + "," + event.preTerminalYield().get(0).value());
+		String classString = "class=" + tokenClass + ",";
+		List<String> updatedFeatures = new ArrayList<String>();
+		for(String feature:features)
+			updatedFeatures.add(classString + feature);
+	
+		FeatureVector fv = new FeatureVector(updatedFeatures);
+		return fv;
+    }
+
+    public List<Datum> setFeaturesTrain(List<Example> data) {
+    	List<Datum> newData = new ArrayList<Datum>();
+	
+		for (Example ex : data) {
+			if(printDebug || printAnnotations) LogInfo.logs("\n-------------------- " + ex.id + "---------------------");
+			for(CoreMap sentence : ex.gold.get(SentencesAnnotation.class)) {
+				//IdentityHashSet<Tree> entityNodes = Utils.getEntityNodesFromSentence(sentence);
+				if(printDebug){
+					LogInfo.logs(sentence);
+					sentence.get(TreeCoreAnnotations.TreeAnnotation.class).pennPrint();
+				}
+				if(printAnnotations) {
+					LogInfo.logs("---Events--");
+					for(EventMention event: sentence.get(EventMentionsAnnotation.class))
+						LogInfo.logs(event.getValue());
+					LogInfo.logs("---Entities--");
+					for(EntityMention entity: sentence.get(EntityMentionsAnnotation.class)) {
+						if(entity.getTreeNode() != null)
+							LogInfo.logs(entity.getTreeNode() + ":" + entity.getTreeNode().getSpan());
+						else
+							LogInfo.logs("Couldn't find node:" + entity.getValue());
+					}
+				}
+				//SemanticGraph dependencies = sentence.get(CollapsedCCProcessedDependenciesAnnotation.class);
+				//LogInfo.logs(dependencies);
+				for(EventMention event: sentence.get(EventMentionsAnnotation.class)) {
+					if(printDebug) LogInfo.logs("-------Event - " + event.getTreeNode()+ "--------");
+					for(Tree node: sentence.get(TreeCoreAnnotations.TreeAnnotation.class)) {
+						if(node.isLeaf()||node.value().equals("ROOT"))
+							continue;
+						
+						String type = Utils.getArgumentMentionRelation(event, node).toString();
+						
+						if (relCount.containsKey(type)) {
+							relCount.put(type, relCount.get(type)+1);
+						} else {
+							relCount.put(type, 1);
+						}
+						
+//						if ((entityNodes.contains(node) && Utils.getArgumentMentionRelation(event, node) != RelationType.NONE)) {// || Utils.isChildOfEntity(entityNodes, node)) {
+//							type = "E";
+//						}
+						if(printDebug) LogInfo.logs(type + " : " + node + ":" + node.getSpan());
+	//					if((entityNodes.contains(node))){// || (Utils.isChildOfEntity(entityNodes, node) && node.value().startsWith("NN"))) {
+	//						type = "E";
+	//					}
+						
+						Datum newDatum = new Datum(sentence, Utils.getText(node), type, node, event.getTreeNode());
+						newDatum.features = computeFeatures(sentence, type, node, event.getTreeNode());
+						if(printFeatures) LogInfo.logs(Utils.getText(node) + ":" + newDatum.features);
+						newData.add(newDatum);
+				}
+			}
+		}
+		if(printDebug) LogInfo.logs("\n------------------------------------------------");
+	}
+
+	return newData;
+    }
+    
+    
+    public List<Datum> setFeaturesTest(CoreMap sentence, Set<Tree> predictedEvents) {
+    	// this is so that the feature factory code doesn't accidentally use the
+    	// true label info
+    	List<Datum> newData = new ArrayList<Datum>();
+    	List<String> labels = ArgumentRelation.getSemanticRoles();
+    	Map<String, Integer> labelIndex = new HashMap<String, Integer>(); 
+    	
+    	int k = 0;
+    	for (String srl : labels) {
+    		labelIndex.put(srl, k++);
+    	}
+
+		for(Tree eventNode: predictedEvents) {
+			for(Tree node: sentence.get(TreeCoreAnnotations.TreeAnnotation.class)) {
+				for (String possibleLabel : labels) {
+					if(node.isLeaf() || node.value().equals("ROOT"))
+						continue;
+					
+					String type = Utils.getArgumentMentionRelation(sentence, eventNode, node).toString();
+					
+					Datum newDatum = new Datum(sentence, Utils.getText(node), type, node, eventNode);
+					newDatum.features = computeFeatures(sentence, possibleLabel, node, eventNode);
+					newData.add(newDatum);
+				}
+		    }
+		}
+    	return newData;
+    }
+    
+    public String getMostCommonRelationType() {
+    	int maximum = -1;
+    	String popularRelation = null;
+    	for (String relType : relCount.keySet()) {
+    		if (relType.equals(RelationType.NONE.toString())) {
+    			continue;
+    		}
+    		if (relCount.get(relType) > maximum) {
+    			maximum = relCount.get(relType);
+    			popularRelation = relType;
+    		}
+    	}
+    	return popularRelation;
+    }
+}
---
+++ NLPCodeBase/src/edu/stanford/nlp/bioprocess/SRLPredictionInferer.java
@@ -0,0 +1,165 @@
+package edu.stanford.nlp.bioprocess;
+
+import java.util.ArrayList;
+import java.util.IdentityHashMap;
+import java.util.List;
+import java.util.Set;
+
+import edu.stanford.nlp.bioprocess.ArgumentRelation.RelationType;
+import edu.stanford.nlp.bioprocess.BioProcessAnnotations.EventMentionsAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreeCoreAnnotations;
+import edu.stanford.nlp.trees.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation;
+import edu.stanford.nlp.util.CoreMap;
+import edu.stanford.nlp.util.IdentityHashSet;
+import edu.stanford.nlp.util.Pair;
+import fig.basic.LogInfo;
+
+public class SRLPredictionInferer extends Inferer {
+	private boolean printDebugInformation = false;
+	List<Datum> prediction = null;
+	
+	public SRLPredictionInferer() {
+		
+	}
+	
+	public SRLPredictionInferer(List<Datum> predictions){
+		this.prediction = predictions;
+	}
+
+	public List<Datum> BaselineInfer(List<Example> examples, Params parameters, FeatureExtractor ff) {
+		List<Datum> predicted = new ArrayList<Datum>();
+		String popularRelation = ((SRLFeatureFactory)ff).getMostCommonRelationType();
+		System.out.println(popularRelation);
+		//EntityFeatureFactory ff = new EntityFeatureFactory();
+		for(Example example:examples) {
+			LogInfo.begin_track("Example %s",example.id);
+			
+			for(CoreMap sentence: example.gold.get(SentencesAnnotation.class)) {
+				Set<Tree> eventNodes = null;
+				if(prediction == null)
+					eventNodes = Utils.getEventNodesFromSentence(sentence).keySet();
+				else
+					eventNodes = Utils.getEventNodesForSentenceFromDatum(prediction, sentence);
+				List<Datum> test = ff.setFeaturesTest(sentence, eventNodes);
+				for(Tree event:eventNodes) {
+					LogInfo.logs("------------------Event " + Utils.getText(event)+"--------------");
+					List<Datum> testDataEvent = new ArrayList<Datum>();
+					for(Datum d:test)
+						if(d.eventNode == event) {
+							testDataEvent.add(d);
+						}
+					List<Datum> testDataWithLabel = new ArrayList<Datum>();
+	
+					for (int i = 0; i < testDataEvent.size(); i += parameters.getLabelIndex().size()) {
+						testDataWithLabel.add(testDataEvent.get(i));
+					}
+					
+					for(Datum d:testDataWithLabel) {
+						if((d.entityNode.value().equals("NP") /*|| d.entityNode.value().startsWith("NN")*/) && Utils.isNodesRelated(sentence, d.entityNode, event)) {
+							d.guessLabel = popularRelation;
+						}
+						else
+							d.guessLabel = RelationType.NONE.toString();
+					}
+					predicted.addAll(testDataWithLabel);
+					
+					LogInfo.logs(sentence);
+					//sentence.get(TreeCoreAnnotations.TreeAnnotation.class).pennPrint();
+					
+					LogInfo.logs("\n---------GOLD ENTITIES-------------------------");
+					for(Datum d:testDataWithLabel) 
+						if(!d.label.equals(RelationType.NONE.toString()))
+							LogInfo.logs(d.entityNode + ":" + d.label);
+					
+					LogInfo.logs("---------PREDICTIONS-------------------------");
+					for(Datum d:testDataWithLabel)
+					{
+						if(!(d.guessLabel.equals(RelationType.NONE.toString()) && d.label.equals(RelationType.NONE.toString()))) {
+							LogInfo.logs(String.format("%-30s [%s], Gold:  %s Predicted: %s", d.word, d.entityNode.getSpan(), d.label, d.guessLabel));
+						}
+					}
+					LogInfo.logs("------------------------------------------\n");
+				}
+			}
+			LogInfo.end_track();
+		}
+		return predicted;
+	}
+	
+	public List<Datum> Infer(List<Example> testData, Params parameters, FeatureExtractor ff) {
+		List<Datum> predicted = new ArrayList<Datum>();
+		//EntityFeatureFactory ff = new EntityFeatureFactory();
+		for(Example ex:testData) {
+			LogInfo.begin_track("Example %s",ex.id);
+			//IdentityHashSet<Tree> entities = Utils.getEntityNodes(ex);
+			
+			for(CoreMap sentence:ex.gold.get(SentencesAnnotation.class)) {
+				if(printDebugInformation ) {
+					LogInfo.logs(sentence);
+					LogInfo.logs(sentence.get(TreeCoreAnnotations.TreeAnnotation.class).pennString());
+					LogInfo.logs(sentence.get(CollapsedCCProcessedDependenciesAnnotation.class));
+				}
+				Set<Tree> eventNodes = null;
+				if(prediction == null)
+					eventNodes = Utils.getEventNodesFromSentence(sentence).keySet();
+				else
+					eventNodes = Utils.getEventNodesForSentenceFromDatum(prediction, sentence);
+				List<Datum> test = ff.setFeaturesTest(sentence, eventNodes);
+				
+				for(Tree event:eventNodes) {
+					LogInfo.logs("------------------Event: " + Utils.getText(event)+"--------------");
+					List<Datum> testDataEvent = new ArrayList<Datum>();
+					for(Datum d:test)
+						if(d.eventNode == event) {
+							//LogInfo.logs(d.entityNode);
+							testDataEvent.add(d);
+						}
+					List<Datum> testDataWithLabel = new ArrayList<Datum>();
+	
+					for (int i = 0; i < testDataEvent.size(); i += parameters.getLabelIndex().size()) {
+						testDataWithLabel.add(testDataEvent.get(i));
+					}
+					MaxEntModel viterbi = new MaxEntModel(parameters.getLabelIndex(), parameters.getFeatureIndex(), parameters.getWeights());
+					viterbi.decodeForEntity(testDataWithLabel, testDataEvent);
+					
+					IdentityHashMap<Tree, Pair<Double, String>> map = new IdentityHashMap<Tree, Pair<Double, String>>();
+	
+					for(Datum d:testDataWithLabel) {
+						if (Utils.subsumesEvent(d.entityNode, sentence)) {
+							map.put(d.entityNode, new Pair<Double, String>(0.0, "O"));
+						} else {
+							map.put(d.entityNode, new Pair<Double, String>(d.getProbability(), d.guessLabel));
+						}
+					}
+					
+					DynamicProgramming dynamicProgrammer = new DynamicProgramming(sentence, map, testDataWithLabel);
+					dynamicProgrammer.calculateLabels();
+					
+					predicted.addAll(testDataWithLabel);
+					
+					LogInfo.logs(sentence);
+					//sentence.get(TreeCoreAnnotations.TreeAnnotation.class).pennPrint();
+					
+					LogInfo.logs("\n---------GOLD ENTITIES-------------------------");
+					for(Datum d:testDataWithLabel) 
+						if(d.label.equals("E"))
+							LogInfo.logs(d.entityNode + ":" + d.label);
+					
+					LogInfo.logs("---------PREDICTIONS-------------------------");
+					for(Datum d:testDataWithLabel)
+						if(d.guessLabel.equals("E") || d.label.equals("E"))
+							LogInfo.logs(String.format("%-30s [%s], Gold:  %s Predicted: %s", d.word, d.entityNode.getSpan(), d.label, d.guessLabel));
+					LogInfo.logs("------------------------------------------\n");
+				}
+			}
+			LogInfo.end_track();
+
+		}
+		return predicted;
+	}
+	
+}
