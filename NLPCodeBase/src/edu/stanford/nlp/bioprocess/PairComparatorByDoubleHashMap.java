521331dedc9bde2602af6960f74c9612e6a0c930
Rishita Anubhai <rishita@stanford.edu>
2013-03-19 16:51:22 -0700

    SRL Generate permutations

---
+++ NLPCodeBase/src/edu/stanford/nlp/bioprocess/PairComparatorByDoubleHashMap.java
@@ -0,0 +1,19 @@
+package edu.stanford.nlp.bioprocess;
+
+import java.util.Comparator;
+import java.util.IdentityHashMap;
+
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.util.Pair;
+
+public class PairComparatorByDoubleHashMap implements Comparator<Pair<IdentityHashMap<Tree, String>, Double>> {
+
+    public int compare(Pair<IdentityHashMap<Tree, String>, Double> pr1, Pair<IdentityHashMap<Tree, String>, Double> pr2) {
+        //return pr2.second.compareTo(pr1.second);
+        if (pr2.second > pr1.second) {
+        	return 1;
+        } else {
+        	return -1;
+        }
+    }
+}
