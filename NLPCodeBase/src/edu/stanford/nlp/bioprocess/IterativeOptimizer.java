256ec856c32bcd1f2915bd909cf0ff11e52094b4
joberant <joberant@stanford.edu>
2013-09-25 19:29:56 -0700

    allow one event

--- NLPCodeBase/src/edu/stanford/nlp/bioprocess/IterativeOptimizer.java
+++ NLPCodeBase/src/edu/stanford/nlp/bioprocess/IterativeOptimizer.java
@@ -22,7 +22,7 @@ public class IterativeOptimizer {
 	public static final String ANSI_PURPLE = "\u001B[35m";
 	public static final String ANSI_CYAN = "\u001B[36m";
 	public static final String ANSI_WHITE = "\u001B[37m";
-	
+
 	public Pair<Triple<Double, Double, Double>, Triple<Double, Double, Double>> optimize(List<Example> train, List<Example> test, boolean useLexicalFeatures) {
 		LogInfo.begin_track("Basiccc trigger prediction");
 		Learner eventLearner = new Learner();
@@ -31,18 +31,18 @@ public class IterativeOptimizer {
 		Params param = eventLearner.learn(train, eventFeatureFactory);
 		List<BioDatum> predicted = inferer.Infer(test, param, eventFeatureFactory);
 		Triple<Double, Double, Double> triple = Scorer.score(predicted);
-		
+
 		LogInfo.logs("Score: Basic trigger prediction - " + triple);
 		LogInfo.end_track();
-		
+
 		Learner entityLearner = new Learner();
 		FeatureExtractor entityFeatureFactory = new EntityFeatureFactory(useLexicalFeatures);
-		
+
 		Inferer entityInferer = new EntityStandaloneInferer();
 		FeatureExtractor entityStandaloneFeatureFactory = new EntityStandaloneFeatureFactory(useLexicalFeatures);
 		Params entityStandaloneParams = entityLearner.learn(train, entityStandaloneFeatureFactory);
 		List<BioDatum> predictedStandaloneEntities = entityInferer.Infer(test, entityStandaloneParams, entityStandaloneFeatureFactory);
-		
+
 		Triple<Double, Double, Double> entityTriple = null;
 		for(int i = 0; i < 1; i++) {
 			LogInfo.begin_track("Entity prediction");
@@ -50,48 +50,48 @@ public class IterativeOptimizer {
 			Params entityParams = entityLearner.learn(train, entityFeatureFactory);
 			List<BioDatum> predictedEntities = entityInferer.Infer(test, entityParams, entityFeatureFactory);
 			entityTriple = Scorer.scoreEntities(test, predictedEntities);
-			
+
 			LogInfo.logs("Score: Entity prediction - " + entityTriple);
 			LogInfo.end_track();
-			
+
 			predictedEntities.addAll(predictedStandaloneEntities);
-			
+
 			LogInfo.begin_track("Extended trigger prediction");
 			inferer = new EventPredictionInferer(predictedEntities);
 			eventFeatureFactory = new EventExtendedFeatureFactory(useLexicalFeatures);
 			param = eventLearner.learn(train, eventFeatureFactory);
 			predicted = inferer.Infer(test, param, eventFeatureFactory);
 			triple = Scorer.score(predicted);
-			
+
 			LogInfo.logs("Score: Extended trigger prediction - " + triple);
 			LogInfo.end_track();
 			//break;
 		}
-		
+
 		entityInferer = new EntityPredictionInferer(predicted);
 		Params entityParams = entityLearner.learn(train, entityFeatureFactory);
 		List<BioDatum> predictedEntities = entityInferer.Infer(test, entityParams, entityFeatureFactory);
 		entityTriple = Scorer.scoreEntities(test, predictedEntities);
-		
+
 		LogInfo.logs("Entity prediction - " + entityTriple);
-		
+
 		return new Pair<Triple<Double, Double, Double>, Triple<Double, Double, Double>>(triple, entityTriple);
 	}
-	
+
 	public Pair<Triple<Double, Double, Double>, Triple<Double, Double, Double>> runPipelinePrediction(List<Example> train, List<Example> test, boolean useLexicalFeatures, String model) {
 		FeatureExtractor eventFeatureFactory = new EventFeatureFactory(useLexicalFeatures);
 		Inferer inferer = new EventPredictionInferer();
 		Params param = (Params) Utils.readObject(Main.EVENT_STANDALONE_MODEL);
 		List<BioDatum> predicted = inferer.Infer(test, param, eventFeatureFactory);
 		Triple<Double, Double, Double> triple = Scorer.score(predicted);
-		
+
 		FeatureExtractor entityFeatureFactory = new EntityFeatureFactory(useLexicalFeatures);
-		
+
 		Inferer entityInferer = new EntityStandaloneInferer();
 		FeatureExtractor entityStandaloneFeatureFactory = new EntityStandaloneFeatureFactory(useLexicalFeatures);
 		Params entityStandaloneParams = (Params) Utils.readObject(Main.ENTITY_STANDALONE_MODEL);
 		List<BioDatum> predictedStandaloneEntities = entityInferer.Infer(test, entityStandaloneParams, entityStandaloneFeatureFactory);
-		
+
 		Triple<Double, Double, Double> entityTriple = null;
 		for(int i = 0; i < 1; i++) {
 			LogInfo.begin_track("Entity prediction");
@@ -99,38 +99,38 @@ public class IterativeOptimizer {
 			Params entityParams = (Params) Utils.readObject(Main.ENTITY_MODEL);
 			List<BioDatum> predictedEntities = entityInferer.Infer(test, entityParams, entityFeatureFactory);
 			entityTriple = Scorer.scoreEntities(test, predictedEntities);
-			
+
 			LogInfo.logs("Score: Entity prediction - " + entityTriple);
 			LogInfo.end_track();
-			
+
 			predictedEntities.addAll(predictedStandaloneEntities);
-			
+
 			LogInfo.begin_track("Extended trigger prediction");
 			inferer = new EventPredictionInferer(predictedEntities);
 			eventFeatureFactory = new EventExtendedFeatureFactory(useLexicalFeatures);
 			param = (Params) Utils.readObject(Main.EVENT_MODEL);
 			predicted = inferer.Infer(test, param, eventFeatureFactory);
 			triple = Scorer.score(predicted);
-			
+
 			LogInfo.logs("Score: Extended trigger prediction - " + triple);
 			LogInfo.end_track();
 			//break;
 		}
-		
+
 		entityInferer = new EntityPredictionInferer(predicted);
 		Params entityParams = (Params) Utils.readObject(Main.ENTITY_MODEL);
 		List<BioDatum> predictedEntities = entityInferer.Infer(test, entityParams, entityFeatureFactory);
 		entityTriple = Scorer.scoreEntities(test, predictedEntities);
-		
+
 		LogInfo.logs("Entity prediction - " + entityTriple);
-		
+
 		//Learner eventRelationLearner = new Learner();
 		EventRelationFeatureFactory eventRelationFeatureFactory = new EventRelationFeatureFactory(useLexicalFeatures, model);
 		EventRelationInferer relationInferer = new EventRelationInferer("global");
-		
+
 		Params eventParam = (Params) Utils.readObject(Main.EVENT_RELATION_GLOBAL_MODEL);
 		List<BioDatum> result = new ArrayList<BioDatum>();
-		
+
 		for(Example ex:test) {
 			LogInfo.logs("Example" + ex.id);
 			LogInfo.logs("Gold Events:");
@@ -145,24 +145,24 @@ public class IterativeOptimizer {
 					Span evtSpan = new Span(span.getSource(), span.getTarget() + 1);
 					LogInfo.logs("\t" + d.eventNode);
 					ArgumentMention mention = new EventMention(Utils.getText(d.eventNode) + "_" + eventsInExample.size(), d.sentence, evtSpan);
-	                IndexedWord head = Utils.findDependencyNode(d.sentence, d.eventNode);
-	              
-	                mention.setHeadInDependencyTree(head);
-	                mention.setTreeNode(d.eventNode);
+					IndexedWord head = Utils.findDependencyNode(d.sentence, d.eventNode);
+
+					mention.setHeadInDependencyTree(head);
+					mention.setTreeNode(d.eventNode);
 					eventsInExample.add((EventMention)mention);
 				}
 			}
-			
+
 			result.addAll(relationInferer.PipelineInfer(ex, eventsInExample, eventParam, eventRelationFeatureFactory, model,
 					true, true, true, true, 0.0,0.5,0,0,1.0,0.0, 0.5));
 		}
 		Triple<Double, Double, Double> resultTriple;
-		
+
 		resultTriple = Scorer.scoreEventRelationsPipeline(test, result);
-		
+
 		return new Pair<Triple<Double, Double, Double>, Triple<Double, Double, Double>>(resultTriple, new Triple<Double, Double, Double>(0.0,0.0,0.0));
 	}
-	
+
 	public void runInference(List<Example> examples) {
 		boolean useLexicalFeatures = true;
 		String model = "global";
@@ -171,14 +171,14 @@ public class IterativeOptimizer {
 		Params param = (Params) Utils.readObject(Main.EVENT_STANDALONE_MODEL);
 		List<BioDatum> predicted = inferer.Infer(examples, param, eventFeatureFactory);
 		//Triple<Double, Double, Double> triple = Scorer.score(predicted);
-		
+
 		FeatureExtractor entityFeatureFactory = new EntityFeatureFactory(useLexicalFeatures);
-		
+
 		Inferer entityInferer = new EntityStandaloneInferer();
 		FeatureExtractor entityStandaloneFeatureFactory = new EntityStandaloneFeatureFactory(useLexicalFeatures);
 		Params entityStandaloneParams = (Params) Utils.readObject(Main.ENTITY_STANDALONE_MODEL);
 		List<BioDatum> predictedStandaloneEntities = entityInferer.Infer(examples, entityStandaloneParams, entityStandaloneFeatureFactory);
-		
+
 		Triple<Double, Double, Double> entityTriple = null;
 		for(int i = 0; i < 1; i++) {
 			//LogInfo.begin_track("Entity prediction");
@@ -186,38 +186,38 @@ public class IterativeOptimizer {
 			Params entityParams = (Params) Utils.readObject(Main.ENTITY_MODEL);
 			List<BioDatum> predictedEntities = entityInferer.Infer(examples, entityParams, entityFeatureFactory);
 			entityTriple = Scorer.scoreEntities(examples, predictedEntities);
-			
+
 			//LogInfo.logs("Score: Entity prediction - " + entityTriple);
 			//LogInfo.end_track();
-			
+
 			predictedEntities.addAll(predictedStandaloneEntities);
-			
+
 			//LogInfo.begin_track("Extended trigger prediction");
 			inferer = new EventPredictionInferer(predictedEntities);
 			eventFeatureFactory = new EventExtendedFeatureFactory(useLexicalFeatures);
 			param = (Params) Utils.readObject(Main.EVENT_MODEL);
 			predicted = inferer.Infer(examples, param, eventFeatureFactory);
 			//triple = Scorer.score(predicted);
-			
+
 			//LogInfo.logs("Score: Extended trigger prediction - " + triple);
 			//LogInfo.end_track();
 			//break;
 		}
-		
+
 		entityInferer = new EntityPredictionInferer(predicted);
 		Params entityParams = (Params) Utils.readObject(Main.ENTITY_MODEL);
 		List<BioDatum> predictedEntities = entityInferer.Infer(examples, entityParams, entityFeatureFactory);
 		entityTriple = Scorer.scoreEntities(examples, predictedEntities);
-		
+
 		//LogInfo.logs("Entity prediction - " + entityTriple);
-		
+
 		//Learner eventRelationLearner = new Learner();
 		EventRelationFeatureFactory eventRelationFeatureFactory = new EventRelationFeatureFactory(useLexicalFeatures, model);
 		EventRelationInferer relationInferer = new EventRelationInferer("global");
-		
+
 		Params eventParam = (Params) Utils.readObject(Main.EVENT_RELATION_GLOBAL_MODEL);
 		List<BioDatum> result = new ArrayList<BioDatum>();
-		
+
 		for(Example ex:examples) {
 			List<EventMention> eventsInExample = new ArrayList<EventMention>();
 			for(BioDatum d:predicted) {
@@ -226,27 +226,27 @@ public class IterativeOptimizer {
 					Span evtSpan = new Span(span.getSource(), span.getTarget() + 1);
 
 					ArgumentMention mention = new EventMention(Utils.getText(d.eventNode) + "_" + eventsInExample.size(), d.sentence, evtSpan);
-	                IndexedWord head = Utils.findDependencyNode(d.sentence, d.eventNode);
-	              
-	                mention.setHeadInDependencyTree(head);
-	                mention.setTreeNode(d.eventNode);
+					IndexedWord head = Utils.findDependencyNode(d.sentence, d.eventNode);
+
+					mention.setHeadInDependencyTree(head);
+					mention.setTreeNode(d.eventNode);
 					eventsInExample.add((EventMention)mention);
 				}
 			}
-			
-			if (eventsInExample.size() > 1) {
+
+			if(eventsInExample.size() >= 1) {
 				result.addAll(relationInferer.PipelineInfer(ex, eventsInExample, eventParam, eventRelationFeatureFactory, model,
 						true, true, true, true, 0.0,0.5,0,0,1.0,0.0, 0.5));
-				
+
 				System.out.println("\n   Predicted " + ANSI_GREEN + "events" +
-				             ANSI_RESET + " and associated " + ANSI_BLUE + "entities" +
-						     ANSI_RESET + ":");
+						ANSI_RESET + " and associated " + ANSI_BLUE + "entities" +
+						ANSI_RESET + ":");
 				for(BioDatum datumEvent:predicted) {
 					if(datumEvent.guessLabel.equals("E")) {
 						if(datumEvent.getExampleID().equals(ex.id) && datumEvent.guessLabel.equals("E")) {
 							System.out.println(ANSI_GREEN + "\t" + Utils.getText(datumEvent.eventNode));
 						}
-					
+
 						for(BioDatum datumEntity:predictedEntities) {
 							if(datumEntity.guessLabel.equals("E") &&
 									datumEntity.eventNode == datumEvent.eventNode) {
@@ -255,12 +255,13 @@ public class IterativeOptimizer {
 						}
 					}
 				}
-					
-				LogInfo.logs(ANSI_RESET + "\n   Predicted event relations:");
-				for(BioDatum eventRelation:result) {
-					if(!eventRelation.guessLabel.equals("NONE")) {
-						System.out.println(ANSI_CYAN + "\t" + Utils.getText(eventRelation.event1.getTreeNode()) + " - " +
-								Utils.getText(eventRelation.event2.getTreeNode()) + " : " + eventRelation.guessLabel);
+				if(eventsInExample.size() > 1) {
+					LogInfo.logs(ANSI_RESET + "\n   Predicted event relations:");
+					for(BioDatum eventRelation:result) {
+						if(!eventRelation.guessLabel.equals("NONE")) {
+							System.out.println(ANSI_CYAN + "\t" + Utils.getText(eventRelation.event1.getTreeNode()) + " - " +
+									Utils.getText(eventRelation.event2.getTreeNode()) + " : " + eventRelation.guessLabel);
+						}
 					}
 				}
 			}
