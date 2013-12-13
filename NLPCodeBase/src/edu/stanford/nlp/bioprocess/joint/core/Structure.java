7a233472bf5e849636137af59a44ebeec0dd9fe7
Vivek Srikumar <me@svivek.com>
2013-12-13 15:52:25 -0800

    edited Structure.toString -- now only prints non-none information

--- NLPCodeBase/src/edu/stanford/nlp/bioprocess/joint/core/Structure.java
+++ NLPCodeBase/src/edu/stanford/nlp/bioprocess/joint/core/Structure.java
@@ -7,148 +7,160 @@ import edu.illinois.cs.cogcomp.indsup.inference.IStructure;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.util.IntPair;
 
+import edu.stanford.nlp.bioprocess.joint.reader.DatasetUtils;
+
 /**
  * 
  * @author svivek
  */
 public class Structure implements IStructure, Serializable {
 
-	private static final long serialVersionUID = -5015837729312326526L;
-	public final Input input;
-	private final String[] relations;
-	private final String[][] arguments;
-	private final String[] triggers;
+    private static final long serialVersionUID = -5015837729312326526L;
+    public final Input input;
+    private final String[] relations;
+    private final String[][] arguments;
+    private final String[] triggers;
 
-	public Structure(Input input, String[] triggers, String[][] arguments,
-			String[] relations) {
-		this.input = input;
-		this.triggers = triggers;
-		this.arguments = arguments;
-		this.relations = relations;
+    public Structure(Input input, String[] triggers, String[][] arguments,
+                     String[] relations) {
+        this.input = input;
+        this.triggers = triggers;
+        this.arguments = arguments;
+        this.relations = relations;
 
-		validate();
-	}
+        validate();
+    }
 
-	private void validate() {
-		assert triggers.length == input.getNumberOfTriggers();
+    private void validate() {
+        assert triggers.length == input.getNumberOfTriggers();
 
-		assert arguments.length == input.getNumberOfTriggers();
-		for (int triggerId = 0; triggerId < arguments.length; triggerId++) {
-			assert arguments[triggerId].length == input
-					.getNumberOfArgumentCandidates(triggerId);
-		}
+        assert arguments.length == input.getNumberOfTriggers();
+        for (int triggerId = 0; triggerId < arguments.length; triggerId++) {
+            assert arguments[triggerId].length == input
+                .getNumberOfArgumentCandidates(triggerId);
+        }
 
-		assert relations.length == input.getNumberOfEERelationCandidates();
-	}
+        assert relations.length == input.getNumberOfEERelationCandidates();
+    }
 
-	@Override
-	public String toString() {
+    @Override
+    public String toString() {
 
-		// some sort of a to string. This will be very verbose, though
-		StringBuffer sb = new StringBuffer();
+        // some sort of a to string. This will be very verbose, though
+        StringBuffer sb = new StringBuffer();
 
-		for (int triggerId = 0; triggerId < input.getNumberOfTriggers(); triggerId++) {
-			CoreLabel triggerCoreLabel = input.getTriggerCoreLabel(triggerId);
-			sb.append("Event trigger: " + triggerCoreLabel.originalText()
-					+ " (token index" + triggerCoreLabel.index() + ")\t"
-					+ triggers[triggerId] + "\n");
+        for (int triggerId = 0; triggerId < input.getNumberOfTriggers(); triggerId++) {
+            // print only the triggers that are labeled "E"
+            if(!triggers[triggerId].equals(DatasetUtils.OTHER_LABEL)) {
+                CoreLabel triggerCoreLabel = input.getTriggerCoreLabel(triggerId);
+                sb.append("Event trigger: " + triggerCoreLabel.originalText()
+                          + " (token index" + triggerCoreLabel.index() + ")\t"
+                          + triggers[triggerId] + "\n");
 
-			sb.append("Arguments: \n");
-			for (int argId = 0; argId < input
-					.getNumberOfArgumentCandidates(triggerId); argId++) {
+                sb.append("Arguments: \n");
+                for (int argId = 0; argId < input
+                         .getNumberOfArgumentCandidates(triggerId); argId++) {
 
-				sb.append("\t" + arguments[triggerId][argId] + "\t");
-				for (CoreLabel c : input.getArgumentCandidate(triggerId, argId))
-					sb.append(c.originalText() + " ");
-				sb.append("\n");
+                    // print only the arguments not labeled "NONE"
+                    if(!arguments[triggerId][argId].equals(DatasetUtils.NONE_LABEL)) {
+                        sb.append("\t" + arguments[triggerId][argId] + "\t");
+                        for (CoreLabel c : input.getArgumentCandidate(triggerId, argId))
+                            sb.append(c.originalText() + " ");
+                        sb.append("\n");
+                    }
 
-			}
-			sb.append("\n");
-		}
+                }
+                sb.append("\n");
+            }
+        }
 
-		sb.append("Event-event relations:\n");
-		for (int rId = 0; rId < input.getNumberOfEERelationCandidates(); rId++) {
-			IntPair ee = input.getEERelationCandidatePair(rId);
+        sb.append("Event-event relations:\n");
+        for (int rId = 0; rId < input.getNumberOfEERelationCandidates(); rId++) {
 
-			sb.append("\t" + relations[rId] + "\t");
+            IntPair ee = input.getEERelationCandidatePair(rId);
 
-			CoreLabel source = input.getTriggerCoreLabel(ee.getSource());
-			CoreLabel target = input.getTriggerCoreLabel(ee.getTarget());
+            // print only the relations that are not labeled "NONE"
+            if(!relations[rId].equals(DatasetUtils.NONE_LABEL)) {
+                sb.append("\t" + relations[rId] + "\t");
 
-			sb.append("\t" + source.originalText() + "\t" + target.originalText()
-					+ "\n");
-		}
+                CoreLabel source = input.getTriggerCoreLabel(ee.getSource());
+                CoreLabel target = input.getTriggerCoreLabel(ee.getTarget());
 
-		return sb.toString();
-	}
+                sb.append("\t" + source.originalText() + "\t" + target.originalText()
+                          + "\n");
+            }
+        }
 
-	@Override
-	public edu.illinois.cs.cogcomp.indsup.learning.FeatureVector getFeatureVector() {
-		// XXX: This function is not needed for this project
-		return null;
-	}
+        return sb.toString();
+    }
 
-	public FeatureVector getFeatures() {
-		// TODO Go over all assignments and compute feature vector
-	   return FeatureExtractor.getFeatures(this);
-	}
+    @Override
+    public edu.illinois.cs.cogcomp.indsup.learning.FeatureVector getFeatureVector() {
+        // XXX: This function is not needed for this project
+        return null;
+    }
 
-	public String getTriggerLabel(int triggerId) {
-		assert input.isValidTriggerId(triggerId);
-		return triggers[triggerId];
-	}
+    public FeatureVector getFeatures() {
+        // TODO Go over all assignments and compute feature vector
+        return FeatureExtractor.getFeatures(this);
+    }
 
-	public String getArgumentCandidateLabel(int triggerId, int argCandidateId) {
-		assert input.isValidTriggerId(triggerId);
-		assert argCandidateId >= 0 && argCandidateId < arguments[triggerId].length;
-		return arguments[triggerId][argCandidateId];
-	}
+    public String getTriggerLabel(int triggerId) {
+        assert input.isValidTriggerId(triggerId);
+        return triggers[triggerId];
+    }
 
-	public String getArgumentCandidateLabel(int triggerId,
-			IntPair argCandidateSpan) {
-		return getArgumentCandidateLabel(triggerId,
-				input.getArgumentSpanIndex(triggerId, argCandidateSpan));
-	}
+    public String getArgumentCandidateLabel(int triggerId, int argCandidateId) {
+        assert input.isValidTriggerId(triggerId);
+        assert argCandidateId >= 0 && argCandidateId < arguments[triggerId].length;
+        return arguments[triggerId][argCandidateId];
+    }
 
-	public String getEERelationLabel(int trigger1, int trigger2) {
-		assert input.isValidTriggerId(trigger1);
-		assert input.isValidTriggerId(trigger2);
+    public String getArgumentCandidateLabel(int triggerId,
+                                            IntPair argCandidateSpan) {
+        return getArgumentCandidateLabel(triggerId,
+                                         input.getArgumentSpanIndex(triggerId, argCandidateSpan));
+    }
 
-		return relations[input.getEERelationIndex(trigger1, trigger2)];
-	}
+    public String getEERelationLabel(int trigger1, int trigger2) {
+        assert input.isValidTriggerId(trigger1);
+        assert input.isValidTriggerId(trigger2);
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + Arrays.hashCode(arguments);
-		result = prime * result + ((input == null) ? 0 : input.hashCode());
-		result = prime * result + Arrays.hashCode(relations);
-		result = prime * result + Arrays.hashCode(triggers);
-		return result;
-	}
+        return relations[input.getEERelationIndex(trigger1, trigger2)];
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Structure other = (Structure) obj;
-		if (!Arrays.deepEquals(arguments, other.arguments))
-			return false;
-		if (input == null) {
-			if (other.input != null)
-				return false;
-		} else if (!input.equals(other.input))
-			return false;
-		if (!Arrays.equals(relations, other.relations))
-			return false;
-		if (!Arrays.equals(triggers, other.triggers))
-			return false;
-		return true;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Arrays.hashCode(arguments);
+        result = prime * result + ((input == null) ? 0 : input.hashCode());
+        result = prime * result + Arrays.hashCode(relations);
+        result = prime * result + Arrays.hashCode(triggers);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Structure other = (Structure) obj;
+        if (!Arrays.deepEquals(arguments, other.arguments))
+            return false;
+        if (input == null) {
+            if (other.input != null)
+                return false;
+        } else if (!input.equals(other.input))
+            return false;
+        if (!Arrays.equals(relations, other.relations))
+            return false;
+        if (!Arrays.equals(triggers, other.triggers))
+            return false;
+        return true;
+    }
 
 }
